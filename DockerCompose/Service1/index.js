39182e04c1bf9c515ab5336d1465b5cbfa89f2bd
Asbah-Usmani <asbahamjad.usmani@tuni.fi>
2021-09-30 17:21:02 +0500

    Complete Assignment

--- DockerCompose/Service1/index.js
+++ DockerCompose/Service1/index.js
@@ -6,16 +6,16 @@ const app = express();
 app.get('/', (req, res) => {
 
   const str1 = "Hello from " + req.client.remoteAddress + ":" + req.client.remotePort
-  + " to " + req.client.localAddress + ":" + req.client.localPort;
+    + " to " + req.client.localAddress + ":" + req.client.localPort;
 
-  http.get("http://localhost:8002", response => {
+  http.get("http://service2:8002", response => {
     let str2 = '';
-      response.on('data', (chunk) => {
-        str2 += chunk;
-      })
-      response.on('end', () => {
-        res.end(str1 + "\n" + str2);
-      })
+    response.on('data', (chunk) => {
+      str2 += chunk;
+    })
+    response.on('end', () => {
+      res.end(str1 + "\n" + str2);
+    })
       .on("error", err => {
         console.log("error" + err.message)
       })
